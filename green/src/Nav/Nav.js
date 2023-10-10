db01eb46c61bef9eb05420060fc00f4bdb89a485
Filippo de Cristofaro <decristo@usc.edu>
2023-10-10 14:58:48 -0700

    nav adjustments

--- green/src/Nav/Nav.js
+++ green/src/Nav/Nav.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import "../Styles/Nav.css";
-import astronautHelmet from "../assets/astronaut-helmet.png";
-import deadEye from "../assets/dead-eye.png";
-import stack from "../assets/stack.png";
-import envelope from "../assets/envelope.png";
+//import astronautHelmet from "../assets/astronaut-helmet.png";
+//import deadEye from "../assets/dead-eye.png";
+//import stack from "../assets/stack.png";
+//import envelope from "../assets/envelope.png";
 import logo from '../assets/Logo.png'
-
 
 
 export default function Navbar() {
@@ -47,28 +46,31 @@ export default function Navbar() {
     return navClass === navPositionClass;
   };
 
-  const renderNavLink = (to, imgSrc, altText, navClass) => {
+  const renderNavLink = (to, /*imgSrc, altText,*/ navClass, pageTitle) => {
     const isCurrent = isCurrentPage(navClass);
     const linkClass = isCurrent ? "nav-link current" : "nav-link";
 
     return (
       <Link to={to} className={linkClass}>
-        <img src={imgSrc} alt={altText} />
-        {isCurrent && <h1 className="page-title">{pageTitle}</h1>}
+        <h1 className="page-title">{pageTitle}</h1>
       </Link>
     );
   };
 
   return (
-    <nav className={`nav ${navPositionClass}`}>
-      {renderNavLink(
-        "/",
-        astronautHelmet,
-        "astronaut helmet icon",
-        "nav-home"
-      )}
-      {renderNavLink("/impact", stack, "stack icon", "nav-impact")}
-      {renderNavLink("/about", deadEye, "deadEye icon", "nav-about")}
-    </nav>
+    <header className = "navHeader">
+      <img className="logo" src={logo}/>
+      <nav className={`nav ${navPositionClass}`}>
+        {renderNavLink(
+          "/",
+        // astronautHelmet,
+        // "astronaut helmet icon",
+          "nav-home",
+          "HOME"
+        )}
+        {renderNavLink("/impact", /*stack, "stack icon",*/ "nav-impact", "IMPACT")}
+        {renderNavLink("/about", /*deadEye, "deadEye icon",*/ "nav-about", "ABOUT")}
+      </nav>
+    </header>
   );
 }
